9030f3c82d699684da279345f12dae046e1e2dc7
HumbertoCastroHorizonti <humberto.castro@horizonti.digital>
2024-05-06 00:11:12 -0300

    last try

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/gymAttendence/', // Set the base to match your GitHub repository name
+  plugins: [react()]
 })
